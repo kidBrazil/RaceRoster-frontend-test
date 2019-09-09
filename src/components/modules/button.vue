3fc3a8bdcc8bb1422a5ae6e62003e5a938c824dc
Lucas Moreira <lucas@mdev.digital>
2019-09-09 15:00:22 -0400

    Finished roughing in the Styleguide, now to finesse

---
+++ src/components/modules/button.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  router-link(
+    :title="accessibility"
+    :aria-label="accessibility"
+    :class="btnClass"
+    :to="btnRoute")
+    slot
+</template>
+
+<script>
+
+export default {
+  name: 'BaseTemplate',
+  props: [ 'accessibility', 'btnClass', 'btnRoute']
+};
+
+</script>
+
+<style lang="scss">
+
+/*-------------------------------------*/
+/* BASE TEMPLATE Component Styles
+/--------------------------------------*/
+
+
+/*--------------------------------------*/
+
+</style>
